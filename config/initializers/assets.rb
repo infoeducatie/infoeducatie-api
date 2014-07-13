bb8b589f90605b34dd1ba64b01b233bf8531e50c
Bogdan Gaza <bc.gaza@gmail.com>
2014-07-13 23:33:00 +0300

    Added the tilt engine so we can render .haml from the assets folder

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( pages.js )
+
+# render haml templates from the assets folder
+Rails.application.assets.register_engine('.haml', Tilt::HamlTemplate)
