142e56704b00853d5323da84a722c1ad99f9caec
Bogdan Gaza <bc.gaza@gmail.com>
2014-07-13 23:33:48 +0300

    Added all the vendor assets to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,13 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require bootstrap
+//= require angular.min
+//= require angular-route.min
+//= require angular-resource.min
+//= require angular-animate.min
+//= require underscore-min.js
+//= require ng-rails-csrf
