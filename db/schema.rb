c4370fdff54b5bcfbf111539c7d1f2dc0ac74cc9
Bogdan Gaza <bc.gaza@gmail.com>
2014-07-14 08:33:00 +0300

    Added the talk resource (in the original app is named seminars)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140713203708) do
+ActiveRecord::Schema.define(version: 20140714053118) do
 
   create_table "editions", force: true do |t|
     t.datetime "start"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 20140713203708) do
     t.integer  "edited_by"
     t.integer  "version"
     t.integer  "edition_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "talks", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "scheduled_at"
+    t.integer  "duration"
+    t.string   "location"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
