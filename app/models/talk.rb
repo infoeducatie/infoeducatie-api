d6024b5c32423b62fecd035580fdde56691ecc10
Alex Palcuie <alex.palcuie@hootsuite.com>
2014-07-15 20:20:02 +0300

    Add validation for talk #3

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,2 +1,5 @@
 class Talk < ActiveRecord::Base
+  validates :title, :description, :datetime, :duration, :location, presence: true
+  validates :title, length: { minimum: 6, maximum: 80 }
+  validates :location, length: { minimum: 5, maximum: 80 }
 end
