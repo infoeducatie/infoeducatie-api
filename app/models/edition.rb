63590c1fae1267fa57704fc63df38a45c0862c8a
Alex Palcuie <alex.palcuie@hootsuite.com>
2014-07-15 20:11:14 +0300

    Add validation for news #3

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -2,6 +2,6 @@ class Edition < ActiveRecord::Base
   has_many :news
   has_many :pages
 
-  validates :start, :end, :cardinal, presence: true
+  validates :start, :end, :cardinal, :motto, presence: true
   validates :motto, length: { maximum: 100, minimum: 5 }
 end
