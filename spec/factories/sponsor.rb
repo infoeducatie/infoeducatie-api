42cba0a43c3d313e83f92975e2e0f365030f8e39
Robert Dolca <robert.dolca@intel.com>
2015-03-14 16:39:50 +0200

    Shorter logo

--- spec/factories/sponsor.rb
+++ spec/factories/sponsor.rb
@@ -4,6 +4,6 @@ FactoryGirl.define do
   factory :sponsor do
     name Faker::Lorem.word
     # TODO: Change in a default image
-    logo Faker::Lorem.paragraph
+    logo Faker::Lorem.word
   end
 end
