076cba63b0592f0e0320e4242fd43213951fb8ec
Robert Dolca <robert.dolca@gmail.com>
2015-04-03 15:41:11 +0300

    HTML specific route tests removed for contestants

--- spec/routing/contestants_routing_spec.rb
+++ spec/routing/contestants_routing_spec.rb
@@ -7,16 +7,8 @@ RSpec.describe ContestantsController, type: :routing do
       expect(:get => "/contestants").to route_to("contestants#index")
     end
 
-    it "routes to #new" do
-      expect(:get => "/contestants/new").to route_to("contestants#new")
-    end
-
     it "routes to #show" do
       expect(:get => "/contestants/1").to route_to("contestants#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/contestants/1/edit").to route_to("contestants#edit", :id => "1")
     end
 
     it "routes to #create" do
