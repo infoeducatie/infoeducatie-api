a811c695d68d99250afec7eada655e3a9b1a8956
Bogdan Gaza <bc.gaza@gmail.com>
2015-04-05 12:39:34 -0700

    Only render json for create in users:sessions

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
-  before_action :always_request_json
+  before_action :always_request_json, only: [:create]
   skip_before_filter :verify_authenticity_token
   respond_to :html, :json
 end
