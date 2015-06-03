0874d9549d329d44bfb2b835921a20017a52a117
Bogdan Gaza <bc.gaza@gmail.com>
2015-06-03 01:58:24 -0700

    Refactored session spec

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -2,34 +2,40 @@ require 'rails_helper'
 
 RSpec.describe "V1::Sessions", type: :request do
   describe "POST /v1/sign_in" do
-    it "should return token when email and password are valid" do
-      @user = FactoryGirl.create(:confirmed_user)
 
-      post "/v1/sign_in", "user[email]" => "test@user.ro", "user[password]" => "TestP4ssW0rd"
-      expect(response).to have_http_status(200)
+    let(:valid_email) { "test@user.ro" }
+    let(:valid_password) { "TestP4ssW0rd" }
 
-      body = JSON.parse(response.body)
-      expect(body["access_token"]).to eq(@user.access_token)
+    before { @user = FactoryGirl.create(:confirmed_user) }
+
+    context "when resource is valid" do
+      it "responds with 200" do
+        post "/v1/sign_in", "user[email]" => valid_email, "user[password]" => valid_password
+        expect(response).to have_http_status(200)
+
+        body = JSON.parse(response.body)
+        expect(body["access_token"]).to eq(@user.access_token)
+      end
     end
 
-    it "should render an authentication error when email is invalid" do
-      @user = FactoryGirl.create(:confirmed_user)
+    context "when resource has invalid email" do
+      it "reponds with 422" do
+        post "/v1/sign_in", "user[email]" => "trex", "user[password]" => valid_password
+        expect(response).to have_http_status(422)
 
-      post "/v1/sign_in", "user[email]" => "trex", "user[password]" => "TestP4ssW0rd"
-      expect(response).to have_http_status(422)
-
-      body = JSON.parse(response.body)
-      expect(body["error"]).to eq("Invalid login attempt")
+        body = JSON.parse(response.body)
+        expect(body["error"]).to eq("Invalid login attempt")
+      end
     end
 
-    it "should render an authetication error when password is invalid" do
-      @user = FactoryGirl.create(:confirmed_user)
+    context "when resource has invalid password" do
+      it "reponds with 422" do
+        post "/v1/sign_in", "user[email]" => valid_email, "user[password]" => "trex"
+        expect(response).to have_http_status(422)
 
-      post "/v1/sign_in", "user[email]" => "test@user.ro", "user[password]" => "trex"
-      expect(response).to have_http_status(422)
-
-      body = JSON.parse(response.body)
-      expect(body["error"]).to eq("Invalid login attempt")
+        body = JSON.parse(response.body)
+        expect(body["error"]).to eq("Invalid login attempt")
+      end
     end
   end
 end
