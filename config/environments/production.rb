48fd36af400dbeb012ba6077eb5c0143bb6a0945
Bogdan Gaza <bc.gaza@gmail.com>
2015-06-27 23:31:37 -0700

    Add default mailer url as infoeducatie.ro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,8 @@ Rails.application.configure do
   # Set to :debug to see everything in the log.
   config.log_level = :info
 
+  config.action_mailer.default_url_options = { :host => 'infoeducatie.ro' }
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
