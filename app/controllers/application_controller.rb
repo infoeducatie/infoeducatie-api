ecb0db427b6cb508a93ee7a1305944bb91306cda
Bogdan Gaza <bc.gaza@gmail.com>
2015-06-27 23:27:53 -0700

    Turn off JSON error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,23 +31,23 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  rescue_from StandardError do |exception|
-    self.response_body = nil
-    if exception.instance_of? ActiveRecord::RecordNotFound
-      render :json => {:status => 404, :error => "Not Found"},
-             :status => :not_found
-    elsif Rails.env.production?
-      render :json => {:status => 500, :error => "We're sorry, but something went wrong."},
-             :status => :internal_server_error
-    else
-      raise exception
-    end
-  end
+  #rescue_from StandardError do |exception|
+  #  self.response_body = nil
+  #  if exception.instance_of? ActiveRecord::RecordNotFound
+  #    render :json => {:status => 404, :error => "Not Found"},
+  #           :status => :not_found
+  #  elsif Rails.env.production?
+  #    render :json => {:status => 500, :error => "We're sorry, but something went wrong."},
+  #           :status => :internal_server_error
+  #  else
+  #    raise exception
+  #  end
+  #end
 
-  rescue_from ActionController::ParameterMissing do |exception|
-    render :json => {:status => 400, :error => "Required parameter missing: #{exception.param}"},
-           :status => :bad_request
-  end
+  #rescue_from ActionController::ParameterMissing do |exception|
+  #  render :json => {:status => 400, :error => "Required parameter missing: #{exception.param}"},
+  #         :status => :bad_request
+  #end
 
   private
   def authenticate_user_from_token!
