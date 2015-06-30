880005ee0b8def08ec17512772320debcf6ead7b
Robert Dolca <robert.dolca@gmail.com>
2015-07-01 00:18:15 +0300

    Admin config for rights

--- app/models/right.rb
+++ app/models/right.rb
@@ -3,4 +3,11 @@ class Right < ActiveRecord::Base
   validates :user, presence: true
   belongs_to :role
   validates :role, presence: true
+
+  rails_admin do
+    list do
+      field :user
+      field :role
+    end
+  end
 end
