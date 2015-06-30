5ff718888ffb1ac97baf62c4f4a10615f6043576
Robert Dolca <robert.dolca@gmail.com>
2015-07-01 00:18:34 +0300

    Admin config for roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,20 @@
 class Role < ActiveRecord::Base
+  validates :name, presence: true
+
   has_many :rights
   has_many :users, through: :rights
+
+  rails_admin do
+    edit do
+      configure :rights do
+        hide
+      end
+      configure :users do
+        hide
+      end
+    end
+    list do
+      field :name
+    end
+  end
 end
