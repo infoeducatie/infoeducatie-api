e46b79a0f055eaf4339f5bd4663d0fe50bdab53b
Robert Dolca <robert.dolca@gmail.com>
2015-06-30 15:30:30 +0300

    Sex enum for admin field dropdown

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -28,6 +28,10 @@ class Contestant < ActiveRecord::Base
   validates :mentoring_teacher_first_name, presence: true
   validates :mentoring_teacher_last_name, presence: true
 
+  def sex_enum
+    [:male, :female, :undisclosed]
+  end
+
   def first_name
     user.first_name
   end
