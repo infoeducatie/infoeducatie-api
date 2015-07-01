cde9fb28db474e64204b232e169ec5207fb3c664
Robert Dolca <robert.dolca@gmail.com>
2015-07-02 02:11:36 +0300

    Category validations and admin refactoring

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,16 @@
 class Category < ActiveRecord::Base
   has_many :projects
 
-  validates :name, uniqueness: true, :allow_nil => true
+  validates :name, uniqueness: true, presence: true
+
+  rails_admin do
+    list do
+      field :name
+    end
+    edit do
+      configure :projects do
+        hide
+      end
+    end
+  end
 end
