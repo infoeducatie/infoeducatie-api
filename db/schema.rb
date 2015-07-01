ac99c87249a5f5a6a2f4d33134b6e976a1355db0
Bogdan Gaza <bc.gaza@gmail.com>
2015-06-30 20:14:44 -0700

    Added finished field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630224416) do
+ActiveRecord::Schema.define(version: 20150701031420) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 20150630224416) do
     t.integer  "category_id"
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
+    t.boolean  "finished",              default: false
   end
 
   create_table "rights", force: :cascade do |t|
