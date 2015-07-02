7d6a9b886f1085a5374c49060e950b46751963c5
Bogdan Gaza <bc.gaza@gmail.com>
2015-07-02 02:13:59 -0700

    Current logged-out

--- app/controllers/v1/current_controller.rb
+++ app/controllers/v1/current_controller.rb
@@ -1,37 +1,44 @@
 module V1
   class CurrentController < ApplicationController
-    before_action :authenticate_user_from_token!, only: [:index]
 
     respond_to :json
 
     # GET /v1/current
     def index
-      has_contestant = !current_user.contestants.empty?
-
-      projects = (current_user.contestants.map do |contestant|
-        contestant.projects
-      end).flatten
-
-      has_projects = projects.empty?
-      has_finished = projects.map(&:finished).any?
-      edition = Edition.find_by(current: true)
       total_counties = Project.active.joins(:contestants)
                               .count('county', :distinct => true)
-
-      @current = {
-        user: current_user,
-        registration: {
-          has_contestant: has_contestant,
-          has_projects: has_projects,
-          has_finished: has_finished
-        },
-        edition: edition,
-        stats: {
-          total_projects: Project.active.size,
-          total_participants: current_user.contestants.size,
-          total_counties: total_counties
-        }
+      stats = {
+        total_projects: Project.active.size,
+        total_participants: Contestant.joins(:projects).where(:projects => { :approved => true }).size,
+        total_counties: total_counties
       }
+
+      unless current_user.nil?
+        has_contestant = !current_user.contestants.empty?
+
+        projects = (current_user.contestants.map do |contestant|
+          contestant.projects
+        end).flatten
+
+        has_projects = projects.empty?
+        has_finished = projects.map(&:finished).any?
+        edition = Edition.find_by(current: true)
+        total_counties = Project.active.joins(:contestants)
+                                .count('county', :distinct => true)
+
+        @current = {
+          user: current_user,
+          registration: {
+            has_contestant: has_contestant,
+            has_projects: has_projects,
+            has_finished: has_finished
+          },
+          edition: edition
+        }
+      else
+        @current = {}
+      end
+      @current[:stats] = stats
 
       render json: @current
     end
