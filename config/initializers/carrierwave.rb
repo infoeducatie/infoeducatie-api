f90c0218e3eaec93c5d1e915cf52dd512aa0c98c
Robert Dolca <robert.dolca@gmail.com>
2015-07-02 23:57:21 +0300

    Skip AWS keys for test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_access_key_id     => ENV['AWS_S3_KEY'],
     :aws_secret_access_key => ENV['AWS_S3_SECRET'],
     :region                => ENV['AWS_S3_REGION']
-  }
+  } if ENV['AWS_S3_KEY']
 
   config.fog_directory  = ENV['AWS_S3_BUCKET']
   config.fog_public = true
