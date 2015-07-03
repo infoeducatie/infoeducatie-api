832faa39ba891721bbe6efde35b1f72856101692
Robert Dolca <robert.dolca@gmail.com>
2015-07-03 21:01:33 +0300

    Validates project and image presence for screenshots

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -2,8 +2,10 @@ require 'carrierwave/orm/activerecord'
 
 class Screenshot < ActiveRecord::Base
   belongs_to :project
+  validates :project, presence: true
 
   mount_uploader :screenshot, ScreenshotUploader
+  validates_presence_of :screenshot
 
   def url
     screenshot.url unless screenshot.nil?
