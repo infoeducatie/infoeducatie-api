504b0c03129dc36a67f5a1168a9294720c5ed77d
Robert Dolca <robert.dolca@gmail.com>
2015-07-05 02:06:35 +0300

    Reorder migrations afterm merge with master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150704214856) do
+ActiveRecord::Schema.define(version: 20150704230348) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
