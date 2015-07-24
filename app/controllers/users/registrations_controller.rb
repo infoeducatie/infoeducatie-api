14fee340d7489d3a1091f55214db14f0aac2adb5
Robert Dolca <robert.dolca@gmail.com>
2015-07-25 01:18:49 +0300

    Allow newsletter settings for user register and update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -77,12 +77,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # You can put the params you want to permit in the empty array.
   def configure_sign_up_params
-    devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name)
+    devise_parameter_sanitizer.for(:sign_up).push(:first_name,
+                                                  :last_name,
+                                                  :newsletter)
   end
 
   # You can put the params you want to permit in the empty array.
   def configure_account_update_params
-    devise_parameter_sanitizer.for(:account_update).push(:first_name, :last_name)
+    devise_parameter_sanitizer.for(:account_update).push(:first_name,
+                                                         :last_name,
+                                                         :newsletter)
   end
 
   # The path used after sign up.
