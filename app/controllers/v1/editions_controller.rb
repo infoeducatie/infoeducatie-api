4bdb02a38d00e5e96403c86f4b59b4d4897edcae
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-07-31 01:38:06 +0300

    Replace has_contestants with has_projects on edition #169

--- app/controllers/v1/editions_controller.rb
+++ app/controllers/v1/editions_controller.rb
@@ -13,11 +13,13 @@ module V1
         @editions.where(show_results: true)
       end
 
-      if params[:has_contestants] == 'true'
-        @editions.where("contestants_count > 0")
+      @editions = @editions.all
+
+      if params[:has_projects] == 'true'
+        @editions.select! { |edition| edition.projects_count > 0 }
       end
 
-      @editions.all
+      @editions
     end
 
   end
